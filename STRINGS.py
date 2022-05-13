f7c12ab778c91b7359b5d5735ad96bab62a69758
BruSen <nizet@live.nl>
2022-05-13 10:53:52 +0200

    opdracht aangepast na feedback

--- STRINGS.py
+++ STRINGS.py
@@ -5,11 +5,12 @@ goal_1 = 54
 scorers = player_1 + " " + str(goal_0) + ", " + player_2 + " " + str(goal_1)
 report = f'{player_1} scored in the {goal_0}nd minute' '\n' f'{player_2} scored in the {goal_1}th minute'
 player = 'Ronald Koeman'
-first = player.find(" ")
-first_name = player[:first]
-last = player[first+1:]
-last_name_len = len(player[7:13])
-name_short = player[:1] + '. ' + player[7:]
+spatie = player.find(" ")
+first_name = player[:spatie]
+last = player[spatie+1:]
+last_name_len = len(last)
+name_short = player[:1] + '. ' + last
 chant = len(first_name) * (first_name + '! ') 
 chant = chant.rstrip( )
 good_chant = chant[-1] != ' '
+print(last_name_len)
